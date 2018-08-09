82c957402c19988b77e5fd6fb398fe2def716404
sw1024 <1106012118@qq.com>
2018-08-10 00:30:04 +0800

    task:00 添加Makefile

---
+++ base/testbase.cpp
@@ -0,0 +1,17 @@
+/**
+ * @file 	testbase.cpp
+ * $Id$
+ * @brief		
+ * @author 	SunWu
+ * @date 	2018-08-10
+ */
+
+#include "testbase.h"
+
+Base::Base()
+{
+}
+
+Base::~Base()
+{
+}
---
+++ base/testbase.h
@@ -0,0 +1,22 @@
+/**
+ * @file 	testbase.h
+ * $Id$
+ * @brief		
+ * @author 	SunWu
+ * @date 	2018-08-10
+ */
+
+#ifndef _TESTBGASE_H_
+#define _TESTBGASE_H_
+
+#include "wLogger.h"
+
+class Base
+{
+	public:
+		Base();
+		virtual ~Base();
+	private:
+};
+
+#endif
